d7b5bb3ab5b9d3386863acb713eacfa3d0830ab1
Bruno Duarte <bduarte2010@gmail.com>
2015-11-09 18:23:56 -0200

    CRUD Serviços

---
+++ src/models/ServicosModel.java
@@ -0,0 +1,131 @@
+
+package models;
+
+/**
+ *
+ * @author Bruno Duarte
+ * @email bduarte2010@gmail.com
+ */
+public class ServicosModel {
+    
+    private int id;
+    private String nome;
+    private String operadora;
+    private String categoria;
+    private Double valor;
+    private Double comissao;
+    private String status;
+    private String created;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the operadora
+     */
+    public String getOperadora() {
+        return operadora;
+    }
+
+    /**
+     * @param operadora the operadora to set
+     */
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the valor
+     */
+    public Double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the comissao
+     */
+    public Double getComissao() {
+        return comissao;
+    }
+
+    /**
+     * @param comissao the comissao to set
+     */
+    public void setComissao(Double comissao) {
+        this.comissao = comissao;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the created
+     */
+    public String getCreated() {
+        return created;
+    }
+
+    /**
+     * @param created the created to set
+     */
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
