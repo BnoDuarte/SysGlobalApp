1646a69e6c34546140daf1c316cc70de284cf029
Bruno Duarte <bduarte2010@gmail.com>
2015-11-16 18:24:45 -0200

    CRUD Vendas

--- src/controllers/VendasController.java
+++ src/controllers/VendasController.java
@@ -20,7 +20,7 @@ public class VendasController {
         connVendas.conexao();
         
         try {
-            PreparedStatement pst = connVendas.conn.prepareStatement("INSERT INTO vendas (funcionario_id, cliente_id, produto_id, servico_id, formapagto, iccid, imei, tipo_venda, port_movel, port_fixo, ntc_fixo, ntc_movel, ntc_movel_portado, operadora_portada, dt_port_fixo, dt_port_movel, dt_venda, protocolo_contrato, status, created) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())");
+            PreparedStatement pst = connVendas.conn.prepareStatement("INSERT INTO vendas (funcionario_id, cliente_id, produto_id, servico_id, formapagto, iccid, imei, tipo_venda, port_movel, port_fixo, ntc_fixo, ntc_movel, ntc_movel_portado, operadora_port_movel, operadora_port_fixo, dt_port_fixo, dt_port_movel, dt_venda, protocolo_contrato, status, created) VALUES (?, ?, 1, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())");
             
             pst.setInt(1, mod.getFuncionario_id());
             pst.setInt(2, mod.getCliente_id());
@@ -35,18 +35,25 @@ public class VendasController {
             pst.setString(11, mod.getNtc_fixo());
             pst.setString(12, mod.getNtc_movel());
             pst.setString(13, mod.getNtc_movel_portado());
-            pst.setString(14, mod.getOperadora_portada());
-            pst.setString(15, mod.getDt_port_fixo());
-            pst.setString(16, mod.getDt_port_movel());
-            pst.setString(17, mod.getDt_venda());
-            pst.setString(18, mod.getProtocolo_contrato());
-            pst.setString(19, mod.getStatus());
+            pst.setString(14, mod.getOperadora_port_movel());
+            pst.setString(15, mod.getOperadora_port_fixo());
+            pst.setString(16, mod.getDt_port_fixo());
+            pst.setString(17, mod.getDt_port_movel());
+            
+            String venda_dia = mod.getDt_venda().substring(0, 2);
+            String venda_mes = mod.getDt_venda().substring(3, 5);
+            String venda_ano = mod.getDt_venda().substring(6);
+            String dt_vendaBD = venda_ano + "-" + venda_mes + "-" + venda_dia;
+            pst.setString(18, dt_vendaBD);
+            
+            pst.setString(19, mod.getProtocolo_contrato());
+            pst.setString(20, mod.getStatus());
             pst.execute();      
             
             JOptionPane.showMessageDialog(null, "Dados armazenados com sucesso!");
             
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao inserir dados.\nERRO: "+ex);
+            JOptionPane.showMessageDialog(null, "Não foi possível salvar os dados.\nERRO: "+ex);
         }
         
         connVendas.desconecta();
